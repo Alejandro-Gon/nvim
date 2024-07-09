3afd6dc364836dc535a84489259034f3fa6d56a0
Alejandro <alegon.dev@pm.me>
2024-07-09 14:41:11 +0200

    install.sh changes

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env bash
 
 sudo pacman -Syu
-sudo pacman -S neovim ripgrep wl-clipboard
+sudo pacman -S neovim ripgrep wl-clipboard npm
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
