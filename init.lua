9a3181f3e99c98c735d05c7edba1372a9b268b84
Alejandro <alegon.dev@pm.me>
2024-09-16 00:51:50 +0200

    refactor

--- init.lua
+++ init.lua
@@ -1,22 +1,14 @@
--- Settings
-vim.opt.termguicolors = true
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.opt.undofile = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.api.nvim_set_option("clipboard", "unnamedplus")
 vim.api.nvim_set_option("mouse", "")
-vim.opt.nu = true
 vim.opt.relativenumber = true;
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+vim.opt.wrap = false
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
-vim.opt.wrap = false
-vim.opt.hlsearch = false
 
--- lazy.nvim
----- Bootstrap
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -32,14 +24,9 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end
 vim.opt.rtp:prepend(lazypath)
-
----- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
-		"ziglang/zig.vim",
 		"tpope/vim-fugitive",
-		"nvim-treesitter/nvim-treesitter",
-		"tikhomirov/vim-glsl",
 		{
 			"rose-pine/neovim",
 			name = "rose-pine",
@@ -51,104 +38,61 @@ require("lazy").setup({
 			end
 		},
 		{
-			"numToStr/Comment.nvim",
-			config = function()
-				require("Comment").setup()
-			end
-		},
-		{
-			"windwp/nvim-autopairs",
-			config = function()
-				require("nvim-autopairs").setup {}
-			end
-		},
-		{
 			"nvim-telescope/telescope.nvim",
-			dependencies = { { "nvim-lua/plenary.nvim" } }
+			config = function()
+				local builtin = require("telescope.builtin")
+				vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+				vim.keymap.set("n", "<C-f>", function()
+					builtin.grep_string({ search = vim.fn.input("Grep > ") })
+				end)
+			end
 		},
 		{
 			"VonHeikemen/lsp-zero.nvim",
 			dependencies = {
-				{ "williamboman/mason.nvim" },
-				{ "williamboman/mason-lspconfig.nvim" },
-				{ "neovim/nvim-lspconfig" },
-				{ "hrsh7th/nvim-cmp" },
-				{ "hrsh7th/cmp-nvim-lsp" },
-				{ "L3MON4D3/LuaSnip" },
+				"williamboman/mason.nvim",
+				"williamboman/mason-lspconfig.nvim",
+				"neovim/nvim-lspconfig",
+				"hrsh7th/nvim-cmp",
+				"L3MON4D3/LuaSnip",
+				"hrsh7th/cmp-nvim-lsp", -- Comment to remove suggestions
 			},
+			config = function()
+				local lsp_zero = require("lsp-zero")
+				lsp_zero.preset("recommended")
+				lsp_zero.on_attach(function(_, bufnr)
+					lsp_zero.default_keymaps({ buffer = bufnr })
+				end)
+				require("mason").setup()
+				require("mason-lspconfig").setup({
+					ensure_installed = { "lua_ls", "zls", "ols", "rust_analyzer", "vtsls" },
+					handlers = { lsp_zero.default_setup }
+				})
+			end
 		},
 		{
-			'akinsho/flutter-tools.nvim',
-			dependencies = {
-				'nvim-lua/plenary.nvim',
-				'stevearc/dressing.nvim', -- optional for vim.ui.select
-			},
+			"akinsho/flutter-tools.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			config = function()
+				require("telescope").load_extension("flutter")
+				require("flutter-tools").setup {
+					flutter_path = os.getenv("HOME") .. "/flutter/bin/flutter",
+					widget_guides = { enabled = true },
+				}
+			end
 		}
 	},
-	install = { colorscheme = { "rose-pine" } },
-	checker = { enabled = true },
 })
---
-
-local treesitter = require("nvim-treesitter.configs");
-local comments = require("Comment.api");
-local lsp_zero = require("lsp-zero")
-local mason = require("mason")
-local mason_lspconfig = require("mason-lspconfig")
-local builtin = require("telescope.builtin")
-local flutterTools = require("flutter-tools")
-local telescope = require("telescope")
-local lspconfig = require('lspconfig');
-telescope.load_extension("flutter")
-lsp_zero.preset("recommended")
-
--- Keymaps
-vim.keymap.set("x", "p", "P")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>dl", vim.diagnostic.setqflist)
-vim.keymap.set("n", "<leader>/", ":vsplit<cr><C-w>l")
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-/>", comments.toggle.linewise.current)
-vim.keymap.set("n", "<C-_>", comments.toggle.linewise.current)
-vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<C-f>", function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-
-local servers = {
-	rust_analyzer = {
-		["rust-analyzer"] = {
-			checkOnSave = {
-				command = "clippy",
-			},
-		},
-	},
-}
-
--- Configs
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "dart",
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.lua", "*.dart", "*.rs", "*.js", "*.ts", "*.sh", "*.zig", "*.odin" },
 	callback = function()
-		vim.keymap.set("n", "<F5>", telescope.extensions.flutter.commands)
-	end,
+		vim.lsp.buf.format { async = false }
+	end
 })
-
 local runners = {
-	{
-		pattern = "zig",
-		cmd = "zig build run",
-	},
-	{
-		pattern = "odin",
-		cmd = "odin run .",
-	},
-	{
-		pattern = "rust",
-		cmd = "cargo run",
-	},
+	{ pattern = "zig",  cmd = "zig build run" },
+	{ pattern = "odin", cmd = "odin run ." },
+	{ pattern = "rust", cmd = "cargo run" },
 }
 for _, runner in ipairs(runners) do
 	vim.api.nvim_create_autocmd("FileType", {
@@ -162,48 +106,9 @@ for _, runner in ipairs(runners) do
 		end,
 	})
 end
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = {
-		"*.lua",
-		"*.dart",
-		"*.rs",
-		"*.js",
-		"*.ts",
-		"*.sh",
-		"*.zig",
-		"*.odin",
-	},
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "dart",
 	callback = function()
-		vim.lsp.buf.format { async = false }
-	end
+		vim.keymap.set("n", "<F5>", require("telescope").extensions.flutter.commands)
+	end,
 })
-
-lsp_zero.on_attach(function(_, bufnr)
-	lsp_zero.default_keymaps({ buffer = bufnr })
-end)
-
-mason.setup({})
-mason_lspconfig.setup({
-	ensure_installed = vim.tbl_keys(servers),
-	handlers = {
-		lsp_zero.default_setup,
-	},
-
-})
-mason_lspconfig.setup_handlers {
-	function(server_name)
-		lspconfig[server_name].setup {
-			capabilities = capabilities,
-			on_attach = on_attach,
-			settings = servers[server_name],
-		}
-	end
-}
-
-flutterTools.setup {
-	flutter_path = os.getenv("HOME") .. "/flutter/bin/flutter",
-	widget_guides = {
-		enabled = true,
-	},
-}
