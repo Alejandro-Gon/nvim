d61274dd3f38ce04e8e45cb9b7138909643dcc96
Alejandro <alegon.dev@pm.me>
2024-09-18 17:45:33 +0200

    line number added

--- init.lua
+++ init.lua
@@ -8,6 +8,7 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
+vim.opt.nu = true
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
