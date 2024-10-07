8a0f9290db24b7e59429139b965628e6afc4f1d4
Alejandro <alegon.dev@pm.me>
2024-10-07 07:26:16 +0200

    small refactor

--- init.lua
+++ init.lua
@@ -10,6 +10,26 @@ vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
 vim.opt.nu = true
 vim.keymap.set("n", "<C-s>", vim.diagnostic.setqflist)
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = { "*.lua", "*.dart", "*.rs", "*.js", "*.ts", "*.sh", "*.zig", "*.odin", "*.go" },
+	callback = function() vim.lsp.buf.format { async = false } end
+})
+for _, r in ipairs({
+	{ pattern = "zig",  cmd = "zig build run" },
+	{ pattern = "odin", cmd = "odin run ." },
+	{ pattern = "rust", cmd = "cargo run" }
+}) do
+	vim.api.nvim_create_autocmd("FileType", {
+		pattern = r.pattern,
+		callback = function()
+			vim.keymap.set("n", "<F5>", function()
+				vim.cmd.vsplit()
+				vim.cmd.wincmd("l")
+				vim.cmd.terminal(r.cmd)
+			end)
+		end,
+	})
+end
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -27,7 +47,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	spec = {
-		"tpope/vim-fugitive",
 		{
 			"rose-pine/neovim",
 			name = "rose-pine",
@@ -38,18 +57,14 @@ require("lazy").setup({
 				vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 			end
 		},
-		{
-			"windwp/nvim-autopairs",
-			config = function() require("nvim-autopairs").setup {} end
-		},
+		"tpope/vim-fugitive",
+		{ "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end },
 		{
 			"nvim-telescope/telescope.nvim",
 			config = function()
 				local builtin = require("telescope.builtin")
 				vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-				vim.keymap.set("n", "<C-f>", function()
-					builtin.grep_string({ search = vim.fn.input("Grep > ") })
-				end)
+				vim.keymap.set("n", "<C-f>", function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
 			end
 		},
 		{
@@ -81,26 +96,3 @@ require("lazy").setup({
 		},
 	},
 })
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = { "*.lua", "*.dart", "*.rs", "*.js", "*.ts", "*.sh", "*.zig", "*.odin", "*.go" },
-	callback = function()
-		vim.lsp.buf.format { async = false }
-	end
-})
-local runners = {
-	{ pattern = "zig",  cmd = "zig build run" },
-	{ pattern = "odin", cmd = "odin run ." },
-	{ pattern = "rust", cmd = "cargo run" },
-}
-for _, runner in ipairs(runners) do
-	vim.api.nvim_create_autocmd("FileType", {
-		pattern = runner.pattern,
-		callback = function()
-			vim.keymap.set("n", "<F5>", function()
-				vim.cmd.vsplit()
-				vim.cmd.wincmd("l")
-				vim.cmd.terminal(runner.cmd)
-			end)
-		end,
-	})
-end
