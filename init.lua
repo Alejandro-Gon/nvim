b21dcc30e3619d2fbba962d26211b7611f2be7f1
Alejandro <alegon.dev@pm.me>
2025-08-23 18:04:25 +0200

    flutter change

--- init.lua
+++ init.lua
@@ -20,16 +20,16 @@ require("lazy").setup({
 			"rose-pine/neovim", name = "rose-pine",
 			config = function() vim.cmd("colorscheme rose-pine") end
 		},
-		"tpope/vim-fugitive",
 		{
 			"nvim-telescope/telescope.nvim", dependencies = { 'nvim-lua/plenary.nvim' },
 			config = function()
 				vim.keymap.set("n", "<C-s>", function() vim.cmd(":Telescope diagnostics") end)
-				vim.keymap.set("n", "<C-b>", function() vim.cmd(":Telescope buffers") end)
 				vim.keymap.set("n", "<C-p>", function() vim.cmd(":Telescope find_files") end)
 				vim.keymap.set("n", "<C-f>", function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") }) end)
 			end
 		},
+		"tpope/vim-fugitive",
+		"akinsho/flutter-tools.nvim",
 		{
 			"VonHeikemen/lsp-zero.nvim", dependencies = { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig", "hrsh7th/nvim-cmp", "L3MON4D3/LuaSnip", "hrsh7th/cmp-nvim-lsp" },
 			config = function()
@@ -39,15 +39,7 @@ require("lazy").setup({
 				require("mason").setup()
 				require("mason-lspconfig").setup({ ensure_installed = lsps, handlers = { require('lsp-zero').default_setup } })
 				require('cmp').setup({sources = {{name = 'nvim_lsp'}}, snippet = {expand = function(args) vim.snippet.expand(args.body) end}, mapping = require('cmp').mapping.preset.insert({})})
-				vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.dart", "*.js", "*.ts", "*.odin", }, callback = function() vim.lsp.buf.format { async = false } end })
-			end
-		},
-		{
-			"akinsho/flutter-tools.nvim", dependencies = { 'nvim-lua/plenary.nvim', 'stevearc/dressing.nvim' },
-			config = function()
-				require("telescope").load_extension("flutter")
-				require("flutter-tools").setup { flutter_path = "/usr/bin/flutter", widget_guides = { enabled = true } }
-				vim.api.nvim_create_autocmd("FileType", { pattern = "dart", callback = function() vim.keymap.set("n", "<F5>", require("telescope").extensions.flutter.commands) end })
+				vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.dart", "*.ts", "*.odin", }, callback = function() vim.lsp.buf.format { async = false } end })
 			end
 		},
 	},
