dc7d24b436bb47d1c6111029f28879f612632a1d
Alejandro <alegon.dev@pm.me>
2025-11-04 20:12:17 +0100

    opencode added

--- init.lua
+++ init.lua
@@ -51,5 +51,23 @@ require("lazy").setup({
 				vim.api.nvim_create_autocmd("FileType", { pattern = "dart", callback = function() vim.keymap.set("n", "<F5>", require("telescope").extensions.flutter.commands) end })
 			end
 		},
+		{
+			"NickvanDyke/opencode.nvim",
+			dependencies = { { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } }, },
+			config = function()
+				vim.g.opencode_opts = {
+					-- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition".
+				}
+				vim.o.autoread = true
+				vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode" })
+				vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
+				vim.keymap.set({ "n", "x" },    "ga", function() require("opencode").prompt("@this") end,                   { desc = "Add to opencode" })
+				vim.keymap.set({ "n", "t" }, "<C-.>", function() require("opencode").toggle() end,                          { desc = "Toggle opencode" })
+				vim.keymap.set("n",        "<S-C-u>", function() require("opencode").command("session.half.page.up") end,   { desc = "opencode half page up" })
+				vim.keymap.set("n",        "<S-C-d>", function() require("opencode").command("session.half.page.down") end, { desc = "opencode half page down" })
+				vim.keymap.set('n', '+', '<C-a>', { desc = 'Increment', noremap = true })
+				vim.keymap.set('n', '-', '<C-x>', { desc = 'Decrement', noremap = true })
+			end,
+		}
 	},
 })
